76045a22839e48fb1fc0ddab294e01426b1a8da3
wanghf <wanghf@iovdc.com>
2018-07-13 17:21:31 +0800

    daily up

--- flink_in_depth/src/main/scala/org/spafka/watermark.scala
+++ flink_in_depth/src/main/scala/org/spafka/watermark.scala
@@ -2,34 +2,8 @@ package org.spafka
 
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
-
-///**
-//  * This generator generates watermarks assuming that elements come out of order to a certain degree only.
-//  * The latest elements for a certain timestamp t will arrive at most n milliseconds after the earliest
-//  * elements for timestamp t.
-//  */
-//class BoundedOutOfOrdernessGenerator extends AssignerWithPeriodicWatermarks[MyEvent] {
-//
-//  val maxOutOfOrderness: Long = 3500L // 3.5 seconds
-//
-//  var currentMaxTimestamp: Long = _
-//
-//  override def extractTimestamp(element: MyEvent, previousElementTimestamp: Long): Long = {
-//    val timestamp = element.createTime
-//    currentMaxTimestamp = Math.max(timestamp, currentMaxTimestamp)
-//    timestamp;
-//  }
-//
-//  override def getCurrentWatermark(): Watermark = {
-//    // return the watermark as current highest timestamp minus the out-of-orderness bound
-//    new Watermark(currentMaxTimestamp - maxOutOfOrderness);
-//  }
-//}
-
-case class MyEvent(createTime: Long)
-
-
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -40,43 +14,55 @@ import org.apache.flink.util.Collector
 object WatermarkTest {
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
-      System.err.println("USAGE:\nSocketWatermarkTest <hostname> <port>")
-      System.exit(1)
-    }
-
-    val hostName = args(0)
-    val port = args(1).toInt
 
     val configuration = new Configuration
     configuration.setInteger("web.port", 8081)
-    val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration)
+    val env = //StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(configuration)
 
+      StreamExecutionEnvironment.getExecutionEnvironment
+    env.getConfig.setAutoWatermarkInterval(1000)
+
+    println(env.getConfig.getAutoWatermarkInterval)
+    env.setMaxParallelism(4)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-    val input = env.socketTextStream(hostName, port)
-
+    val input = env.socketTextStream(args(0), args(1).toInt)
+    //
+    //    val input = env.addSource(new SourceFunction[String] {
+    //
+    //      @volatile var isRuning = true;
+    //
+    //      override def run(sourceContext: SourceFunction.SourceContext[String]): Unit = {
+    //        var a: Long = 0L;
+    //        while (isRuning) {
+    //          a = a + 1L;
+    //          sourceContext.collect(s"1 ${a}")
+    //          Thread.sleep(10)
+    //        }
+    //      }
+    //
+    //      override def cancel(): Unit = {
+    //        isRuning = false;
+    //      }
+    //    }).setParallelism(1)
     input.map(f => {
       val arr = f.split("\\W")
       val code = arr(0)
       val time = arr(1).toLong
       (code, time)
-    }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[(String, Long)](Time.seconds(0)) {
+    }).setParallelism(1).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[(String, Long)](Time.seconds(10)) {
       override def extractTimestamp(element: (String, Long)): Long = {
 
         println(element)
         element._2
       }
     }).keyBy(_._1)
-      .window(TumblingEventTimeWindows.of(Time.seconds(1)))
+      .window(TumblingEventTimeWindows.of(Time.seconds(1))).allowedLateness(Time.seconds(10))
       .process(new ProcessWindowFunction[(String, Long), String, String, TimeWindow] {
         override def process(key: String, context: Context, elements: Iterable[(String, Long)], out: Collector[String]): Unit = {
-
-
-         println(s"${context.currentWatermark}, ${elements.toList}")
+          println(s"${context.currentWatermark}, ${elements.toList}")
         }
       })
-
     env.execute()
   }
 
