b8719f80295bdeaeae09effedcf78beec48e8c2a
wanghf <wanghf@iovdc.cn>
2018-10-24 17:49:17 +0800

    up zk kafka

--- build-in-cluster/kafka/src/main/java/com/github/spafka/kafkaServer.java
+++ build-in-cluster/kafka/src/main/java/com/github/spafka/kafkaServer.java
@@ -1,7 +1,9 @@
 package com.github.spafka;
 
 import kafka.server.KafkaServerStartable;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -10,6 +12,9 @@ public class kafkaServer {
 
 
     public static void main(String[] args) throws IOException {
+
+        FileUtils.deleteDirectory(new File("/data"));
+
 
         InputStream is = kafkaServer.class.getResourceAsStream("/server.properties");
         Properties p = new Properties();
--- build-in-cluster/zookeeper/src/main/java/com/github/spafka/ZkServer.java
+++ build-in-cluster/zookeeper/src/main/java/com/github/spafka/ZkServer.java
@@ -1,9 +1,11 @@
 package com.github.spafka;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.zookeeper.server.ServerConfig;
 import org.apache.zookeeper.server.ZooKeeperServerMain;
 import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -11,6 +13,10 @@ import java.util.Properties;
 public class ZkServer {
 
     public static void main(String[] args) throws IOException, QuorumPeerConfig.ConfigException {
+
+
+        FileUtils.deleteDirectory(new File("/tmp"));
+
         QuorumPeerConfig config = new QuorumPeerConfig();
         InputStream is = ZkServer.class.getResourceAsStream("/zookeeper.properties");
         Properties p = new Properties();
