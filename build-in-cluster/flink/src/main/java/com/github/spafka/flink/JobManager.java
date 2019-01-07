10b790e509d887fe9554d9833ac92e4962e48513
spafka <spafka@163.com>
2019-01-07 21:15:37 +0800

    add shade

--- build-in-cluster/flink/src/main/java/com/github/spafka/flink/JobManager.java
+++ build-in-cluster/flink/src/main/java/com/github/spafka/flink/JobManager.java
@@ -1,17 +1,19 @@
 package com.github.spafka.flink;
 
-import org.apache.flink.runtime.jobmanager.JobManager$;
+import org.apache.flink.runtime.entrypoint.StandaloneSessionClusterEntrypoint;
 
 public class JobManager {
 
     public static void main(String[] args) {
+
+        System.setProperty("log.file","jobmanager.log");
 
         args = String.format("--configDir %s --executionMode cluster --host localhost --webui-port 8081",
                 Thread.currentThread()
                         .getContextClassLoader()
                         .getResource("flink-conf.yaml")
                         .getFile()+"/..").split(" ");
-        JobManager$.MODULE$.main(args);
+        StandaloneSessionClusterEntrypoint.main(args);
 
     }
 }
